25e5158b0a4cc47a61af3dd4ed914932b39af1a8
Tad Leonard <tadfleonard@gmail.com>
2015-06-17 15:20:46 -0500

    Row mover with wraparound is working. Experimenting with moviepy.

--- mix.py
+++ mix.py
@@ -1,31 +1,44 @@
 from __future__ import print_function
+
 import sys
-import time
+import random
+
 import imread
 import numpy as np
+from moviepy.editor import VideoClip
+from husl import rgb_to_husl
 
 
-def timeit(fn):
-    def new_fn(*args, **kwargs):
-        start = time.time()
-        ret = fn(*args, **kwargs)
-        print("{} {:0.4f}s".format(fn.__name__, time.time() - start))
-        return ret
-    return new_fn
-
-
-@timeit
 def make_grid(img):
     rows = np.sum(img, axis=1)
     cols = np.sum(img, axis=0)
-    print(img.shape)
-    print(rows)
-    print('--')
-    print(rows.shape)
-    print('--')
-    print(cols.shape)
+
+
+def move(img_pixels, travel):
+    """Shift `img_pixels` a distance of `travel` in the positive direction.
+    The pixel array wraps around, so the last pixels will end up being
+    the first pixels."""
+    tail = img_pixels[-travel:].copy()  # pixel array wraps around
+    img_pixels[travel:] = img_pixels[:-travel]  # move bulk of pixels
+    img_pixels[:travel] = tail  # move the saved `tail` into vacated space
+
+
+def _move_random(img):
+    for row in img:
+        move(row, random.choice(range(1, 5)))
+    return img
+
+
+def animate(img, make_frame, duration):
+    animation = VideoClip(make_frame, duration=duration)
+    animation.write_gif("bloop.gif", fps=24)
 
 
 if __name__ == "__main__":
     img = imread.imread(sys.argv[1])
     make_grid(img)
+
+    def rand_move(t):
+        return _move_random(img)
+
+    animate(img, rand_move, duration=3)
