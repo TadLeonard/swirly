87681e273669aa282b0112bb6346260b37d4c314
Tad Leonard <tadfleonard@gmail.com>
2015-08-20 17:22:05 -0500

    made swirlop Cython file 'private' for easier cmdline completion

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import swirl
 
 
 ext_module = Extension(
-    "swirlop",
-    ["swirlop.pyx"],
+    "_swirlop",
+    ["_swirlop.pyx"],
 #    extra_compile_args=['-fopenmp'],
 #    extra_link_args=['-fopenmp'],
     include_dirs=[numpy.get_include()],
