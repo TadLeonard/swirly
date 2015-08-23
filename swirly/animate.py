8f70cb7396aa171ea52d833d2a868bd117b957d8
Tad Leonard <tadfleonard@gmail.com>
2015-08-23 02:06:29 -0500

    separating generic animation tools from concrete, finished animations

--- swirly/animate.py
+++ swirly/animate.py
@@ -2,16 +2,13 @@
 image masks, and movers are used to create frame generators."""
 
 from abc import abstractmethod, ABCMeta
+from functools import partial
 
 import numpy as np
 import nphusl
 
 from . import movers
 from . import effects
-
-
-###################################
-# Effects coupled with pixel movers
 
 
 no_op = lambda x: x
@@ -53,7 +50,7 @@ class Animation(metaclass=ABCMeta):
     def move(self, img, select, travel): ...
     
 
-# Concrete animations
+### Concrete animations
 
 class RubixAnimation(Animation):
     move = move_rubix
@@ -62,22 +59,16 @@ class SwapAnimation(Animation):
     move = move_swap
 
 
-# Preparer functions for views and effects
+### Preparer functions for views, effects, and animations
 
 def reverse(chunks):
     return ((i, s, -travel) for i, s, travel in chunks)
+
 
 def flip(masked_img):
     img, select = masked_img
     rotated = imgmask(np.rot90(img), np.rot90(select))
     return rotated
-
-
-# Specific views, effects, & animations for end user
-
-flipped_view = partial(view, prepare_img=flip)
-clump_effect = partial(effect, clump_cols)
-disperse_effect = partial(clump_effect, prepare_moves=reverse)
 
 
 def make_animation(masked_img, move_magnitudes,
@@ -89,25 +80,11 @@ def make_animation(masked_img, move_magnitudes,
     return _animation
 
 
-# Complete animation makers
+### Filtering pixels
 
-slide_vert = partial(make_animation, animation=SwapAnimation)
-slide_horz = partial(slide_vert, view=flipped_view)
-rubix_vert = partial(make_animation, animation=RubixAnimation)
-rubix_horz = partial(rubix_vert, view=flipped_view)
-
-clump_vert = partial(rubix_vert, effect=clump_effect)
-clump_horz = partial(rubix_horz, effect=clump_effect)
-
-disperse_vert = partial(rubix_vert, effect=disperse_effect)
-disperse_horz = partial(disperse_vert, view=flipped_view)
-
-
-###################
-# Filtering pixels
-
-_or = np.logical_or
-_and = np.logical_and
+# This namedtuple holds a 3D of the image itself and a 2D array of the
+# selected pixels. It gets passed around to effect functions.
+imgmask = namedtuple("img", ["img", "select"])
 
 
 def _choose(chooser, starter, img, *selections):
@@ -123,75 +100,8 @@ mask = partial(_choose, np.logical_and, np.ones)
 mask_or = partial(_choose, np.logical_or, np.zeros)
 
 
-# This namedtuple holds a 3D of the image itself and a 2D array of the
-# selected pixels. It gets passed around to effect functions.
-imgmask = namedtuple("img", ["img", "select"])
+### Merging multiple effects into a single animation
 
-
-def clump_dark(img):
-    hsl = nphusl.to_husl(img)
-    H, _, L = (hsl[..., n] for n in range(3))
-    dark = mask(img, L < 5)
-    travel = (5,)
-    vert = clump_vert(dark, travel)
-    horz = clump_horz(dark, travel)
-    yield from zip_effects(img, horz, vert)
-
-
-def disperse_light(img):
-    hsl = nphusl.to_husl(img)
-    H, _, L = (hsl[..., n] for n in range(3))
-    light = mask(img, L > 80)
-    logging.info("Selection ratio: {:1.1f}%".format(
-                 100 * np.count_nonzero(light) / light.size))
-    travel = (1,)
-    vert = disperse_vert(img, light, travel)
-    horz = disperse_horz(img, light, travel)
-    yield from zip_effects(img, horz, vert)
-
-
-def clump_hues(img):
-    hsl = nphusl.to_husl(img)
-    H, _, L = (hsl[..., n] for n in range(3))
-    light = mask(img, L > 1)
-    travel = (1,)
-    
-    def effects():
-        for selection in select_ranges(H, 50, light):
-            yield clump_vert(selection, travel)
-
-    yield from zip_effects(img, *effects())
-
-
-def select_ranges(select_by, percentile, *extra_filters):
-    selectable = mask(img, *extra_filters) 
-    selectable_values = select_by[selectable.select]
-    min_val = 0
-    for max_pct in range(percentile, 100 + percentile, percentile):
-        max_val = np.percentile(selectable_values, max_pct)
-        selection = mask(selectable.select,
-                         select_by < max_val, select_by > min_val)
-        yield selection
-        min_val = max_val
-
-
-def slide_colors(img):
-    hsl = nphusl.to_husl(img)
-    H, L = hsl[..., 0], hsl[..., 2]
-    light = mask(L > 3)
-    blue = mask(img, light, H > 240, H < 290)
-    red = mask(img, light, _or(H < 40, H > 320))
-    travel = (4,)
-    blue_up = slide_vert(blue, travel)
-    red_right = slide_horz(red, travel)
-    #yield from zip_effects(img, blue_up, red_right)
-    light_right = slide_horz(mask(light), travel)
-    yield from zip_effects(img, light_right)
-
-
-##################################
-# Creating animations from effects
-    
 
 def handle_kb_interrupt(fn):
     @wraps(fn)
