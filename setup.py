1b69b4171181cce4944121a1f9a7658792fa8a08
Tad Leonard <tadfleonard@gmail.com>
2015-08-23 00:35:02 -0500

    setup.py modified for new package structure; script entrypoints

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 import numpy
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Distutils import build_ext
-import swirl
+from swirly.version import __version__
 
 
 ext_module = Extension(
     "_swirlop",
-    ["_swirlop.pyx"],
-#    extra_compile_args=['-fopenmp'],
-#    extra_link_args=['-fopenmp'],
+    ["swirly/_swirlop.pyx"],
     include_dirs=[numpy.get_include()],
+    #extra_compile_args=["-fopenmp"],
+    #extra_link_args=["-fopenmp"],
 )
 
 
 setup(
-    name='swirl',
-    version=swirl.__version__,
-    py_modules=["swirl"],
-    cmdclass={'build_ext': build_ext},
+    name="swirl",
+    version=__version__,
+    packages=["swirly"],
+    cmdclass={"build_ext": build_ext},
     ext_modules=[ext_module],
+    entry_points={
+        'console_scripts': [
+            'swirl = swirly.cli:main',
+        ],
+        'gui_scripts': [
+        ]
+    }
 )
